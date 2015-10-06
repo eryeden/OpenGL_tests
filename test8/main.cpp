c65f53875cdf1a7f1d63151efe4edd13231e9c33
kazuki_kikuti <ery.eden@gmail.com>
2015-10-06 17:47:31 +0900

    add : postprocess23

--- test8/main.cpp
+++ test8/main.cpp
@@ -96,67 +96,36 @@ int main() {
 
 void gl_execute(GLFWwindow *window) {
 
-
-	//必要なのか？
-	//GLuint VertexArrayID;
-	//glGenVertexArrays(1, &VertexArrayID);
-	//glBindVertexArray(VertexArrayID);
-
-
-
-	//GLuint shader_programme_id = LoadShaders("C:/Users/B4/Source/Repos/OpenGL_tests/test6/test_vs.glsl", "C:/Users/B4/Source/Repos/OpenGL_tests/test6/test_fs.glsl");
-   //GLuint shader_nontex_id = LoadShaders("C:/Users/B4/Source/Repos/OpenGL_tests/test6/test_vs.glsl", "C:/Users/B4/Source/Repos/OpenGL_tests/test6/test_fs.glsl");
-   //GLuint shader_usetex_id = LoadShaders("C:/Users/B4/Source/Repos/OpenGL_tests/test6/test_vs.glsl", "C:/Users/B4/Source/Repos/OpenGL_tests/test6/test_fs.glsl");
-
-
 #ifdef KIKUTI_HOME
 	GLuint shader_nontex_id = LoadShaders("C:/Users/ery/Documents/local_devel/OpenGL_tests/GLSL/StandardShading_vs_non_tex.glsl"
 		, "C:/Users/ery/Documents/local_devel/OpenGL_tests/GLSL/StandardShading_fs_non_tex.glsl");
 	GLuint shader_usetex_id = LoadShaders("C:/Users/ery/Documents/local_devel/OpenGL_tests/GLSL/StandardShading_vs.glsl"
 		, "C:/Users/ery/Documents/local_devel/OpenGL_tests/GLSL/StandardShading_fs.glsl");
+
+	GLuint shader_postprocess = LoadShaders(
+		"C:/Users/ery/Documents/local_devel/OpenGL_tests/GLSL/pp_vs.glsl"
+		, "C:/Users/ery/Documents/local_devel/OpenGL_tests/GLSL/pp_fs.glsl"
+		);
+
 #else
 	GLuint shader_nontex_id = LoadShaders("C:/Users/B4/Source/Repos/OpenGL_tests/GLSL/StandardShading_vs_non_tex.glsl"
 		, "C:/Users/B4/Source/Repos/OpenGL_tests/GLSL/StandardShading_fs_non_tex.glsl");
 	GLuint shader_usetex_id = LoadShaders("C:/Users/B4/Source/Repos/OpenGL_tests/GLSL/StandardShading_vs.glsl"
 		, "C:/Users/B4/Source/Repos/OpenGL_tests/GLSL/StandardShading_fs.glsl");
+
+	//ポストプロセシング用シェーダー
+	GLuint shader_postprocess_id = LoadShaders(
+		"C:/Users/B4/Source/Repos/OpenGL_tests/GLSL/pp_vs.glsl"
+		, "C:/Users/B4/Source/Repos/OpenGL_tests/GLSL/pp_fs.glsl"
+		);
 #endif
 
-	//#if (_MSC_VER == 1900)	 //Visual Studio 2015
-	//	GLuint shader_nontex_id = LoadShaders("C:/Users/ery/Documents/local_devel/OpenGL_tests/GLSL/StandardShading_vs_non_tex.glsl"
-	//		, "C:/Users/ery/Documents/local_devel/OpenGL_tests/GLSL/StandardShading_fs_non_tex.glsl");
-	//	GLuint shader_usetex_id = LoadShaders("C:/Users/ery/Documents/local_devel/OpenGL_tests/GLSL/StandardShading_vs.glsl"
-	//		, "C:/Users/ery/Documents/local_devel/OpenGL_tests/GLSL/StandardShading_fs.glsl");
-	//#endif
-	//
-	//#if (_MSC_VER == 1800)	 //Visual Studio 2013
-	//	GLuint shader_nontex_id = LoadShaders("C:/Users/B4/Source/Repos/OpenGL_tests/GLSL/StandardShading_vs_non_tex.glsl"
-	//		, "C:/Users/B4/Source/Repos/OpenGL_tests/GLSL/StandardShading_fs_non_tex.glsl");
-	//	GLuint shader_usetex_id = LoadShaders("C:/Users/B4/Source/Repos/OpenGL_tests/GLSL/StandardShading_vs.glsl"
-	//		, "C:/Users/B4/Source/Repos/OpenGL_tests/GLSL/StandardShading_fs.glsl");
-	//#endif
+
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
 
 
-
-		//座標変換について
-		/*
-		  CPU側では変換行列を用意する
-		  すべての変換を示す行列を用意出来たらこれをGPU側に転送する
-		  シェーダはこの変換行列を頂点に掛け算して変換後座標を得る
-		  つまり各座標に対しての変換はGPU側；シェーダ側で処理される
-		  これらは並列に実行されるため高速な変換が可能になる
-		 */
-		 /*
-		   モデル空間からワールド空間への変換を定める[Model Coordinates -> World Coordinates]
-		   ワールド空間からカメラ空間への変換を定める[World Coordinates -> Camera Coordinates]
-		   カメラ空間から同次空間への変換を定める[Camera Coordinates -> Homogeneous Coordinates]
-		  */
-
-		  /*
-			#1: MVP(Model View Projection)行列を定める Projection * View * Model
-			#2: GLSL(Shader side)に渡す
-			#3: シェーダ側でMVPを用いて頂点の変換を行う
-
-		   */
 
 	mat4 Model = mat4(1.0f); // 単位行列
 	Model = mat4(
@@ -175,9 +144,6 @@ void gl_execute(GLFWwindow *window) {
 
 	mat4 MVP = Projection * View * Model; //これをGLSLに渡す
 
-
-
-
 	//シェーダ中のMVPへのハンドルを取得
 	GLuint MatrixID_MVP = glGetUniformLocation(shader_nontex_id, "MVP");
 	//GLuint TextureID = glGetUniformLocation(shader_programme_id, "my_texture_sampler");
@@ -193,11 +159,10 @@ void gl_execute(GLFWwindow *window) {
 		黄色：山吹？ vec3(1, 0.85, 0)
 			  FANUC? vec3(0.964, 0.714, 0)
 	*/
-
 #ifdef KIKUTI_HOME
 	imptr.Load("C:/Users/ery/Documents/local_devel/OpenGL_tests/models/t1.stl", vec3(0.8, 0.2, 0.4)); //#cc00000
 	imptr1.Load("C:/Users/ery/Documents/local_devel/OpenGL_tests/models/Asm.stl", vec3(0, 0.71, 0.101));
-	 //imptr.Load("C:/Users/ery/Documents/local_devel/OpenGL_tests/models/cube1.obj", vec3(1, 1, 1)); //#cc00000
+	//imptr.Load("C:/Users/ery/Documents/local_devel/OpenGL_tests/models/cube1.obj", vec3(1, 1, 1)); //#cc00000
 #else
 	imptr.Load("C:/Users/B4/Source/Repos/OpenGL_tests/models/stoadmm.stl", vec3(0.964, 0.714, 0)); //#cc00000
 	imptr1.Load("C:/Users/B4/Source/Repos/OpenGL_tests/models/Asm.stl", vec3(0, 0.71, 0.101));
@@ -205,71 +170,127 @@ void gl_execute(GLFWwindow *window) {
 	//imptr.Load("C:/Users/B4/OneDrive/Documents/Models/PersonalComputer.stl", vec3(0, 0.8, 0.8)); //#cc00000
 
 #endif
-
-
-//#if (_MSC_VER == 1900)	 //Visual Studio 2015
-//	imptr.Load("C:/Users/ery/Documents/local_devel/OpenGL_tests/models/t1.stl", vec3(0.8, 0.2, 0.4)); //#cc00000
-//	//imptr.Load("C:/Users/ery/Documents/local_devel/OpenGL_tests/models/cube1.obj", vec3(1, 1, 1)); //#cc00000
-//#endif
-//
-//#if (_MSC_VER == 1800)	 //Visual Studio 2013
-//	imptr.Load("C:/Users/B4/Source/Repos/OpenGL_tests/models/stoadmm.stl", vec3(0.8, 0.2, 0.4)); //#cc00000
-//	//imptr.Load("C:\Users\B4\Source\Repos\OpenGL_tests\models/cube1.obj", vec3(1, 1, 1)); //#cc00000
-//#endif
-
-
-	/*
-	軸変換
-	X -> X
-	Y -> Z
-	Z -> -Y
-	*/
-
-
 	int width, height;
+	glfwGetWindowSize(window, &width, &height);
 
-	// glEnable (GL_CULL_FACE); // cull face
-	// glCullFace (GL_BACK); // cull back face
-	// glFrontFace (GL_CW); // GL_CCW for counter clock-wise
+	//#######################INITIALIZE POSTPROCESS##############################
+	/*
+		テクスチャ、レンダバッファ、FBOの初期化を行う
+	*/
+	// ---------------------------------------------
+	// Render to Texture - specific code begins here
+	// ---------------------------------------------
+
+	// The framebuffer, which regroups 0, 1, or more textures, and 0 or 1 depth buffer.
+	GLuint FramebufferName = 0;
+	glGenFramebuffers(1, &FramebufferName);
+	glBindFramebuffer(GL_FRAMEBUFFER, FramebufferName);
+
+	// The texture we're going to render to
+	GLuint renderedTexture;
+	glGenTextures(1, &renderedTexture);
+
+	// "Bind" the newly created texture : all future texture functions will modify this texture
+	glBindTexture(GL_TEXTURE_2D, renderedTexture);
+
+	// Give an empty image to OpenGL ( the last "0" means "empty" )
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
+
+	// Poor filtering
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+	// The depth buffer
+	GLuint depthrenderbuffer;
+	glGenRenderbuffers(1, &depthrenderbuffer);
+	glBindRenderbuffer(GL_RENDERBUFFER, depthrenderbuffer);
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, width, height);
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthrenderbuffer);
+
+	//// Alternative : Depth texture. Slower, but you can sample it later in your shader
+	//GLuint depthTexture;
+	//glGenTextures(1, &depthTexture);
+	//glBindTexture(GL_TEXTURE_2D, depthTexture);
+	//glTexImage2D(GL_TEXTURE_2D, 0,GL_DEPTH_COMPONENT24, 1024, 768, 0,GL_DEPTH_COMPONENT, GL_FLOAT, 0);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); 
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+	// Set "renderedTexture" as our colour attachement #0
+	glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, renderedTexture, 0);
+
+	//// Depth texture alternative : 
+	//glFramebufferTexture(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, depthTexture, 0);
+
+
+	// Set the list of draw buffers.
+	GLenum DrawBuffers[1] = { GL_COLOR_ATTACHMENT0 };
+	glDrawBuffers(1, DrawBuffers); // "1" is the size of DrawBuffers
+
+								   // Always check that our framebuffer is ok
+	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+		printf("FB failed\n");
+		//return false;
+
+
+	// The fullscreen quad's FBO
+	static const GLfloat g_quad_vertex_buffer_data[] = {
+		-1.0f, -1.0f, 0.0f,
+		1.0f, -1.0f, 0.0f,
+		-1.0f,  1.0f, 0.0f,
+		-1.0f,  1.0f, 0.0f,
+		1.0f, -1.0f, 0.0f,
+		1.0f,  1.0f, 0.0f,
+	};
+
+	GLuint quad_vertexbuffer;
+	glGenBuffers(1, &quad_vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, quad_vertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_quad_vertex_buffer_data), g_quad_vertex_buffer_data, GL_STATIC_DRAW);
+
+	// Create and compile our GLSL program from the shaders
+	//GLuint quad_programID = LoadShaders("pp_vs.glsl", "pp_fs.glsl");
+	GLuint texID = glGetUniformLocation(shader_postprocess_id, "renderedTexture");
+
+
+	//#######################INITIALIZE POSTPROCESS##############################
+
+
+
+
 
 	while (!glfwWindowShouldClose(window)) {
 		_update_fps_counter(window);
 
 		glfwGetWindowSize(window, &width, &height);
+
+
+		//############################ON RESHAPE POSTPROCESS################################
+		//スクリーンサイズ変更時にテクスチャサイズ、深度バッファ（レンダバッファ）サイズの変更が必要
+		// On reshape
+		//テクスチャのリサイズ
+		glBindTexture(GL_TEXTURE_2D, renderedTexture);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+		glBindTexture(GL_TEXTURE_2D, 0);
+		//レンダバッファ（深度バッファ）のリサイズ
+		glBindRenderbuffer(GL_RENDERBUFFER, depthrenderbuffer);
+		glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT16, width, height);
+		glBindRenderbuffer(GL_RENDERBUFFER, 0);
+		//############################ON RESHAPE POSTPROCESS################################
+
+
+		//レンダリング開始時にFBOをバインドする。
+		//そうするとレンダバッファではなくて、テクスチャに色フレームが描画されるようになる
+		glBindFramebuffer(GL_FRAMEBUFFER, FramebufferName);
 		glViewport(0, 0, width, height);
-		// wipe the drawing surface clear
-
-		// mat4 Model = mat4(1.0f); // 単位行列
-		// mat4 View = lookAt(
-		//    vec3(1, 1, 1),
-		//    vec3(0, 0 ,0),
-		//    vec3(0, 1, 0)
-		//    );
-		// //どこからどこを見るか　ワールド座標において
-		Projection = perspective(45.0f, (float)width / (float)height, 0.1f, 1000.0f); //glPerspective
-
-		// mat4 MVP = Projection * View * Model; //これをGLSLに渡す
-
 
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		//glUseProgram(shader_programme_id); // Set the shader programme that should be "in use"
 		glUseProgram(shader_nontex_id);
-		//glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
 
-		//Bind iur texture in Texture Unit 0
-		//glActiveTexture(GL_TEXTURE0);
-		//glBindTexture(GL_TEXTURE_2D, texture);
-		//Set our my_texture_sampler sampler to user Texture Unit 0
-		//glUniform1i(TextureID, 0);
-
-		//glBindVertexArray (vao);
-		// // draw points 0-3 from the currently bound VAO with current in-use shader
-		//glDrawArrays (GL_TRIANGLES, 0, 3);
-		// // update other events like input handling
-
-		// //Draw second VAO
-		// glBindVertexArray(vao2);
-		// glDrawArrays(GL_TRIANGLES, 0, 3);
+		Projection = perspective(45.0f, (float)width / (float)height, 0.1f, 1000.0f); //glPerspective
 
 		Model = mat4(
 			1.0f, 0.0f, 0.0f, 0.0f
@@ -283,18 +304,16 @@ void gl_execute(GLFWwindow *window) {
 		glUniformMatrix4fv(MatrixID_M, 1, GL_FALSE, &Model[0][0]);
 		glUniformMatrix4fv(MatrixID_V, 1, GL_FALSE, &View[0][0]);
 
-
 		lightPos = vec3(4, 4, 4);
 		glUniform3f(Vector3ID_LightPosition, lightPos.x, lightPos.y, lightPos.z);
 
 		imptr.Render();
 
-
 		Model = mat4(
 			1.0f, 0.0f, 0.0f, 0.0f
 			, 0.0f, 1.0f, 0.0f, 0.0f
 			, 0.0f, 0.0f, 1.0f, 0.0f
-			, 0.0f, 0.0f, 0.0f, 1.0f
+			, 0.0f, 0.0f, 3.0f, 1.0f
 			);
 		MVP = Projection * View * Model; //これをGLSLに渡す
 		glUniformMatrix4fv(MatrixID_MVP, 1, GL_FALSE, &MVP[0][0]);
@@ -307,15 +326,54 @@ void gl_execute(GLFWwindow *window) {
 		imptr1.Render();
 
 
-		glfwPollEvents();
-		// put the stuff we've been drawing onto the display
+
+		//レンダリング終了時にFBOのバインドを解除する
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+		glViewport(0, 0, width, height);
+
+		//ここからポストプロセシング
+		glClearColor(0.0, 0.0, 0.0, 1.0);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		
+		//ポストプロセスに使用するシェーダーを起動
+		glUseProgram(shader_postprocess_id);
+
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, renderedTexture);
+		glUniform1i(texID, 0);
+
+		//頂点バッファ
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, quad_vertexbuffer);
+		glVertexAttribPointer(
+			0
+			, 3
+			, GL_FLOAT
+			, GL_FALSE
+			, 0
+			, (void *)0
+			);
+		 //描画
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDisableVertexAttribArray(0);
+
+		
 		glfwSwapBuffers(window);
+		glfwPollEvents();
 
 		if (GLFW_PRESS == glfwGetKey(window, GLFW_KEY_ESCAPE)) {
 			glfwSetWindowShouldClose(window, 1);
 		}
 
 	}
+
+	//リソースの開放
+	glDeleteFramebuffers(1, &FramebufferName);
+	glDeleteTextures(1, &renderedTexture);
+	glDeleteRenderbuffers(1, &depthrenderbuffer);
+	glDeleteBuffers(1, &quad_vertexbuffer);
+	glDeleteVertexArrays(1, &VertexArrayID);
 }
 
 
